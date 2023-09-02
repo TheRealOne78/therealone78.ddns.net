3672e7d7a3260045264783ee2c2e94fb3f06106d
TheRealOne78 <bajcsielias78@gmail.com>
2023-09-02 11:15:31 +0000

    advguildgen: put variables in brackets for security

--- resources/advguild/genhtml.sh
+++ resources/advguild/genhtml.sh
@@ -28,7 +28,7 @@ FOOTER_TEMPLATE_PATH="./templates/footer.html"
 DIR_DEPTH=0
 FILE_NAME_JSON=""
 
-ADVGUILD_URL_BASE="$ADVGUILD_HTTP_PATH"
+ADVGUILD_URL_BASE="${ADVGUILD_HTTP_PATH}"
 
 TEMP_GEN_FILENAME=""
 ADVGUILD_JSON=""
@@ -39,9 +39,9 @@ PERMISSIONS="755"
 # =FUNCTIONS======================= #
 
 checkerr() {
-  if [ "$exitcode" -ne "0" ]; then
-		printf "$ERR Error: Non-zero exit code \`$exitcode'. Exiting...\n";
-		exit $exitcode
+  if [ "${exitcode}" -ne "0" ]; then
+		printf "$ERR Error: Non-zero exit code \`${exitcode}'. Exiting...\n";
+		exit ${exitcode}
 	else
 		return 0;
 	fi
@@ -54,46 +54,46 @@ genparse() {
 		# Print container opener
 
 		## Directory
-		if [ -d "$file" ]; then
-		  echo "<div class=\"dir file-shower\">" >> "$TEMP_GEN_FILENAME"
+		if [ -d "${file}" ]; then
+		  echo "<div class=\"dir file-shower\">" >> "${TEMP_GEN_FILENAME}"
 		else
 		  # Check by extention
 		  extension=${file##*.}
-			extension=$(echo $extension | tr '[:upper:]' '[:lower:]')
-      case $extension in
+			extension=$(echo ${extension} | tr '[:upper:]' '[:lower:]')
+      case "${extension}" in
 
 				## Archives
 				"tar" | "gz" | "xz" | "bz" | "bz2" | "lz" | "lz4" | "lzma" | "lzo" | "zip" | "7z" | "rar" | "iso" | "br" | "sz" | "zst")
-				echo "<div class=\"archive file-shower\">" >> "$TEMP_GEN_FILENAME"
+				echo "<div class=\"archive file-shower\">" >> "${TEMP_GEN_FILENAME}"
 				filetype="Archive"
 				;;
 
         ## Executables (except Java JAR)
 				"appimage" | "run" | "sh" | "py" | "exe" | "dmg" | "apk" | "out" | "app" | "cmd" | "bat" | "command" | "vbs" | "ps1")
-				echo "<div class=\"exe file-shower\">" >> "$TEMP_GEN_FILENAME"
+				echo "<div class=\"exe file-shower\">" >> "${TEMP_GEN_FILENAME}"
 				filetype="Executable" ;;
 
 				## Audio
 				"pcm" | "wav" | "aiff" | "mp3" | "aac" | "ogg" | "wma" | "flac" | "alac")
-				echo "<div class=\"audio file-shower\">" >> "$TEMP_GEN_FILENAME"
+				echo "<div class=\"audio file-shower\">" >> "${TEMP_GEN_FILENAME}"
 				filetype="Audio media"
 				;;
 
 				## Video
 				"mp4" | "mov" | "avi" | "flv" | "mkv" | "wmv" | "avchd" | "webm" | "mpeg" | "m4a")
-				echo "<div class=\"video file-shower\">" >> "$TEMP_GEN_FILENAME"
+				echo "<div class=\"video file-shower\">" >> "${TEMP_GEN_FILENAME}"
 				filetype="Video media"
 				;;
 
 				## Java JAR
 				"jar")
-				echo "<div class=\"jar file-shower\">" >> "$TEMP_GEN_FILENAME"
+				echo "<div class=\"jar file-shower\">" >> "${TEMP_GEN_FILENAME}"
 				filetype="Java executable"
 				;;
 
 				## Default
 				*)
-				echo "<div class=\"file file-shower\">" >> "$TEMP_GEN_FILENAME"
+				echo "<div class=\"file file-shower\">" >> "${TEMP_GEN_FILENAME}"
 				filetype="File"
 				;;
 		  esac
@@ -102,58 +102,58 @@ genparse() {
 	  EXITCODE=$?; checkerr;
 
 		# Print title
-		printf "<a href=\"$ADVGUILD_URL_BASE/${file}\"><b>$(basename "$file")</b></a><br>\n" >> $TEMP_GEN_FILENAME
+		printf "<a href=\"${ADVGUILD_URL_BASE}/${file}\"><b>$(basename "${file}")</b></a><br>\n" >> "${TEMP_GEN_FILENAME}"
 
 		# Print description
-		toprint="$(jq -r "${FILE_NAME_JSON}.\"$(basename "$file")\".Description" $ADVGUILD_JSON)"
+		toprint="$(jq -r "${FILE_NAME_JSON}.\"$(basename "${file}")\".Description" ${ADVGUILD_JSON})"
 		if [ "${toprint}" != "null" ]; then
-		  printf "<br>\n${toprint}\n<br>\n" >> $TEMP_GEN_FILENAME
+		  printf "<br>\n${toprint}\n<br>\n" >> ${TEMP_GEN_FILENAME}
 		fi
 
 		# Print archive message if archive.
-		if [ -f "${file}" ] && [ "$filetype" = "Archive" ]; then
-      printf "<br>\nTo access the contents of this archive file, decompress it with a tool like <a href=\"https://www.7-zip.org/\"><b>7-zip</b></a>.<br>" >> $TEMP_GEN_FILENAME
+		if [ -f "${file}" ] && [ "${filetype}" = "Archive" ]; then
+      printf "<br>\nTo access the contents of this archive file, decompress it with a tool like <a href=\"https://www.7-zip.org/\"><b>7-zip</b></a>.<br>" >> "${TEMP_GEN_FILENAME}"
 		fi
 
 		# Misc breaks for files
-		printf "\n<br>\n" >> "$TEMP_GEN_FILENAME"
+		printf "\n<br>\n" >> "${TEMP_GEN_FILENAME}"
 
-		if [ -d "$file" ]; then
-			ADVGUILD_URL_BASE="$ADVGUILD_URL_BASE/${file}"
-			FILE_NAME_JSON="${FILE_NAME_JSON}.\"$(basename "$file")\""
-			cd "$file"
+		if [ -d "${file}" ]; then
+			ADVGUILD_URL_BASE="${ADVGUILD_URL_BASE}/${file}"
+			FILE_NAME_JSON="${FILE_NAME_JSON}.\"$(basename "${file}")\""
+			cd "${file}"
 			((DIR_DEPTH++))
 			genparse
 			cd ..
 			((DIR_DEPTH--))
-			ADVGUILD_URL_BASE="$(dirname "$ADVGUILD_URL_BASE")"
+			ADVGUILD_URL_BASE="$(dirname "${ADVGUILD_URL_BASE}")"
 			FILE_NAME_JSON="${FILE_NAME_JSON%.*}"
 		fi
 
 
 		# File details
-		if [ -f "$file" ]; then
-			printf "<a href=\"$ADVGUILD_URL_BASE/${file}\" download>Download</a> | " >> $TEMP_GEN_FILENAME
-			printf "Type: $filetype | " >> $TEMP_GEN_FILENAME
-		  printf "Date and time: $(stat -c "%y" "$file" | awk '{split($2,a,"."); print $1, a[1]}')" >> $TEMP_GEN_FILENAME
+		if [ -f "${file}" ]; then
+			printf "<a href=\"${ADVGUILD_URL_BASE}/${file}\" download>Download</a> | " >> "${TEMP_GEN_FILENAME}"
+			printf "Type: ${filetype} | " >> "${TEMP_GEN_FILENAME}"
+		  printf "Date and time: $(stat -c "%y" "${file}" | awk '{split($2,a,"."); print $1, a[1]}')" >> "${TEMP_GEN_FILENAME}"
 		fi
 
 		# Close container tag
-		printf "</div>\n<br>\n\n" >> "$TEMP_GEN_FILENAME"
+		printf "</div>\n<br>\n\n" >> "${TEMP_GEN_FILENAME}"
 	done
 }
 
 generate() {
-	curdir=$(pwd)
+	curdir="$(pwd)"
 
 	# CHDIR into advguild root directory
-  cd $ADVGUILD_ABSOLUTE_PATH
+  cd "${ADVGUILD_ABSOLUTE_PATH}"
 	exitcode=$?; checkerr;
 
 	# Now generate stuff
 	genparse
 
-	cd $curdir
+	cd "${curdir}"
   unset curdir
 }
 
@@ -161,38 +161,38 @@ main() {
 	TEMP_GEN_FILENAME="$(pwd)/advguild.html.notcomplete"
 	ADVGUILD_JSON="$(pwd)/advguild.json"
 
-	printf "$INFO Reading \`$HEADER_TEMPLATE_PATH'...\n"
-	echo "<!DOCTYPE HTML>" > "$TEMP_GEN_FILENAME"
-	cat "$HEADER_TEMPLATE_PATH" >> "$TEMP_GEN_FILENAME"
+	printf "$INFO Reading \`${HEADER_TEMPLATE_PATH}'...\n"
+	echo "<!DOCTYPE HTML>" > "${TEMP_GEN_FILENAME}"
+	cat "${HEADER_TEMPLATE_PATH}" >> "${TEMP_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 
 	printf "$INFO Starting to generate HTML code...\n"
-	echo "<!--Beginning of generated HTML code-->" >> "$TEMP_GEN_FILENAME"
+	echo "<!--Beginning of generated HTML code-->" >> "${TEMP_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 
 	generate
 
-	echo "<br>Last synced on $(cat "./lastsync"). This page was generated on $(date) by $(hostname).<br>" >> $TEMP_GEN_FILENAME
+	echo "<br>Last synced on $(cat "./lastsync"). This page was generated on $(date) by $(hostname).<br>" >> "${TEMP_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 
-	echo "<!--End of generated HTML code-->" >> "$TEMP_GEN_FILENAME"
+	echo "<!--End of generated HTML code-->" >> "${TEMP_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 	printf "$INFO Done generating HTML code.\n"
 
-	printf "$INFO Reading \`$FOOTER_TEMPLATE_PATH'...\n"
-	cat "$FOOTER_TEMPLATE_PATH" >> "$TEMP_GEN_FILENAME"
+	printf "$INFO Reading \`${FOOTER_TEMPLATE_PATH}'...\n"
+	cat "${FOOTER_TEMPLATE_PATH}" >> "${TEMP_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 
-	printf "$INFO Moving \'$TEMP_GEN_FILENAME' to final destination, \'${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}'...\n"
-	mv "$TEMP_GEN_FILENAME" "${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}"
+	printf "$INFO Moving \'${TEMP_GEN_FILENAME}' to final destination, \'${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}'...\n"
+	mv "${TEMP_GEN_FILENAME}" "${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 
-	printf "$INFO Changing file permissions...\n"
-	chmod "$PERMISSIONS" "${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}"
+	printf "$INFO Changing file permissions as ${PERMISSIONS}...\n"
+	chmod "${PERMISSIONS}" "${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 
-	printf "$INFO Changing file group:owner...\n"
-	chown "$OWNER" "${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}"
+	printf "$INFO Changing file group and owner as ${OWNER}...\n"
+	chown "${OWNER}" "${HTTPD_ROOT_ABSOLUTE_PATH}${FINAL_GEN_FILENAME}"
 	exitcode=$?; checkerr;
 
 	printf "$INFO Done.\n";
