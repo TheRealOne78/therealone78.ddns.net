9765e63930810f745987bd0b0e8ddc5141ae065c
TheRealOne78 <bajcsielias78@gmail.com>
2024-08-01 00:13:20 +0300

    add some stuff I don't remember what was created for

---
+++ webring.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <!-- Primary Meta Tags -->
+    <title>Webring</title>
+    <link rel="icon" href="/resources/img/icons/favico.svg" type="image/x-icon">
+    <link rel="stylesheet" href="/resources/css/social-media.css?version=1.1">
+    <link rel="stylesheet" href="/resources/css/main.css?version=1.1">
+    <meta name="title" content="Webring">
+    <meta name="description" content="Webring">
+    <meta content="#000" data-react-helmet="true" name="theme-color">
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://therealone78.ddns.net/">
+    <meta property="og:title" content="Webring">
+    <meta property="og:description" content="Webring">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://therealone78.ddns.net/">
+    <meta property="twitter:title" content="Webring">
+    <meta property="twitter:description" content="Webring">
+  </head>
+
+  <body>
+    <div id="wrap">
+      <div id="main">
+        <?php
+          $HEADER_TITLE="Webring";
+          include($_SERVER['DOCUMENT_ROOT'] . "/resources/header.php");
+        ?>
+        <div class="dw_cool">
+          <h4 id="description">Description</h4>
+          <p>This page lists some webrings.</p>
+          <h4 id="disclaimer">Disclaimer</h4>
+          Page under construction. Don't expect any deadline...<br><br>
+          <div align="center">
+            <pre>
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⠤⠤⠀⠀⠀⠤⠤⠤⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠢⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⠒⢉⣁⡀⠉⡶⢄⡀⠀⣠⠞⠁⢀⠔⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⢦⡀⠀⠈⠢⡀⢠⠚⣀⡴⠊⣉⡀⠈⠉⢦⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⡗⠀⢀⣀⣙⢷⠃⢀⣿⠞⠁⠀⣴⠇⠀⠀⠀⢀⡔⠀⠀⠀⠀⠀⢢⠀⠀⠀⠀⠀⠹⣦⠀⠀⠘⢿⡆⢹⡵⣋⣀⠀⠀⠀⢸⣆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢠⠋⡀⠀⠀⢀⣀⡏⣠⡿⠗⠀⢀⠎⠉⠀⠀⠀⢠⡾⠀⠀⠀⠀⠀⠀⠈⣷⡀⠀⠀⠀⠀⣘⡳⡀⠙⠪⣿⣤⣯⠓⣤⣄⡀⠀⠤⡉⢆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⢣⠞⣀⠴⠚⢋⡭⡿⣻⣇⠀⢀⢎⡀⢡⠇⢀⢠⡯⡇⢠⠀⠀⠀⠀⠘⠁⡇⢷⣀⣀⡘⢯⠤⢄⢱⠀⠀⡟⣏⠻⡲⣄⠀⠉⠲⠄⠈⢪⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡗⠁⠊⠁⡠⠞⢁⡜⣱⡿⡗⢕⡜⣠⡁⡎⠀⢠⡿⢁⠀⡇⠀⠀⠀⠀⢵⠀⣿⠈⣷⡀⠀⠘⡆⠈⠦⢇⠘⢸⣿⡄⢣⠈⠣⡀⠀⠀⠀⠀⢱⡀⠀⠀⠀
+⠀⠀⠀⠀⡜⠁⠀⢀⠞⢀⢔⡵⣳⣿⣷⠁⠀⠇⠀⢸⠁⢀⡿⠁⢸⠀⡇⠀⠀⠀⠀⡾⡇⡿⠀⠘⢿⡄⠀⢻⠀⠀⢸⣠⠃⣿⣇⢨⠣⡀⠙⢄⠀⠀⠀⠀⢇⠀⠀⠀
+⠀⠀⢀⣴⠁⠀⢠⠃⣰⡷⠋⢰⠋⢸⣯⢣⣸⠀⠀⣼⠤⣞⠏⠉⢹⣯⡇⠀⠀⠀⣼⡣⣇⡟⠉⠉⠉⢿⡦⢼⡇⠀⢸⠁⢠⣏⢹⠛⠀⠈⢦⣨⢦⠀⠀⠀⠘⡦⡀⠀
+⠀⡠⣫⡏⠀⢠⠃⡰⠋⠀⠀⠀⠀⣧⢹⡄⢹⠀⠀⣿⣸⠏⠀⠀⠀⢻⢻⠀⠀⣰⡿⠃⣿⠇⠀⣀⡀⠈⢷⡘⡇⠀⢸⣀⣾⡟⢸⠀⠀⠀⠀⠙⢧⢣⠀⠀⠀⢹⢌⠢
+⠀⡔⢡⠃⢠⠇⠰⠁⠀⠀⠀⠀⢠⡇⠸⣿⣾⠀⠀⣿⣏⣶⣿⣿⢿⣿⡎⡆⡴⠓⠁⠀⠟⣲⣿⣿⣿⣿⣶⣷⡇⠀⢸⣿⢿⠃⢘⡄⠀⠀⠀⠀⠀⢣⣣⠀⠀⠘⡌⠁
+⡜⠀⢸⠀⡜⢀⡇⠀⠀⠀⠀⠀⢸⠇⠀⣿⣽⠀⠀⡿⣾⠋⣿⣻⣿⣿⠙⠈⠀⠀⠀⠀⠀⢉⣟⣻⣿⣿⡟⣷⡇⠀⢸⢯⡏⠀⠈⡇⠀⠀⠀⠀⠀⠀⢫⡄⠀⠀⡇⠀
+⠀⠀⡇⢀⠃⡞⡇⠀⠀⠀⠀⠀⣾⠀⢀⢿⣿⠀⠀⣧⠘⠆⢻⣛⢋⡝⠀⠀⠀⠀⠀⠀⠀⠀⢫⣛⣋⡝⠐⢡⡧⠀⢸⣜⡇⠀⠀⢶⠀⠀⠀⠀⠀⠀⠈⣷⠀⠀⢸⠀
+⠀⠀⢇⢸⢸⠱⣱⡀⠀⠀⠀⠀⠇⠀⡸⡸⠹⠀⠀⡟⡄⠀⣠⣌⣥⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡰⣴⡀⠀⢸⡇⠀⢸⠎⢇⠀⠀⢸⡀⠀⠀⠀⠀⠀⢠⢿⡄⠀⠈⠀
+⠀⠀⢸⢸⢸⠀⠙⢷⣄⠀⠀⢸⠁⢠⢧⠇⠀⠀⠀⣿⣳⡀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⣜⠇⠀⢸⠀⠸⡀⠀⠐⡇⠀⠀⠀⠀⠀⣼⠟⡇⠀⡆⠀
+⠀⠀⠈⡞⣸⠀⠀⠀⠀⠉⠁⡏⠀⡎⡜⠀⠀⠀⠀⣇⢧⠳⡀⠀⠀⠀⠀⠐⠤⠄⠀⠀⠂⠀⠀⠀⠀⢀⡜⡿⡄⠀⡆⠀⠀⣇⠀⠀⢿⠀⠀⠀⣀⠞⠃⠀⡇⢰⠁⠀
+⠀⠀⠀⠘⣧⢧⠀⠀⠀⠀⡸⠁⡜⢰⠃⠀⠀⠀⠀⣯⠻⢧⣈⠳⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠚⡇⣼⢿⠀⠀⡇⠀⠀⠸⡀⠀⠨⡇⠈⠉⠀⠀⠀⢸⢠⠇⠀⠀
+⠀⠀⠀⠀⠹⡝⠷⣀⠀⢠⠇⠈⢁⡏⠀⠀⠀⠀⠀⣿⠀⠀⠈⠀⣠⢪⡏⠑⠢⠄⠠⠔⠚⢹⠷⡀⠀⠉⠁⢸⢠⠇⡇⠀⠀⠀⢇⠀⠀⢵⠀⠀⠀⠀⢀⡾⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠀⠀⢀⡎⠀⠀⡜⠀⠀⢀⡄⠀⠀⣇⣤⣤⠖⠉⢸⠈⠳⢄⡀⠀⠀⣀⠴⠊⢸⠉⠓⢦⣤⣿⠃⠀⡇⠀⠀⠀⠘⡄⠀⠐⡇⠀⠀⠤⠊⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⡜⠀⠀⡼⠁⠀⢀⡼⠔⡆⠀⣧⣿⡇⠀⠀⢾⣦⡀⠀⢉⡷⡍⠁⢀⣤⡞⠀⠀⠈⣿⣸⠀⢰⠣⢆⡀⠀⠀⢣⠀⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡾⢁⠄⡼⠁⠀⡰⠁⠀⠀⠁⢠⡛⣿⠀⠀⠀⠸⣿⣿⣶⣮⣴⣾⣾⣿⣿⠇⠀⠀⠀⢸⡿⠀⢸⠀⠀⠈⢢⡀⠈⡆⠀⠈⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⡞⣠⠋⡰⠁⠀⢠⠇⠀⠀⠀⡇⢸⢿⡟⠀⠀⠀⠀⢻⡿⠻⣿⣿⣿⡟⣿⡟⠀⠀⠀⠀⢸⡇⡄⢸⠀⠀⠀⠀⢇⠀⠸⡀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣰⢏⣔⠇⡰⠁⢠⠔⢹⠀⠀⠸⡀⡇⣼⢸⡇⠀⠀⠀⠀⠀⠳⣀⣿⣿⣿⣠⠎⠀⠀⠀⠀⠀⢸⡇⡇⢸⠀⣰⠀⠀⠸⡄⠀⢳⠀⠀⢹⡄⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⡾⡡⢋⠎⡰⠁⣴⠋⠀⠈⠀⠀⠀⢣⣇⢿⢸⣧⠀⠀⠀⠀⠀⠀⠘⢿⣿⠟⠁⠀⠀⠀⠀⠀⠀⣿⢃⡇⢸⢠⠃⠀⢀⠀⠙⢦⠈⣆⠀⠀⢳⡀⠀⠀⠀⠀⠀
+⠀⢠⢋⠞⠀⡜⢰⢡⣎⢿⣦⡀⠀⠀⠀⠀⢆⢿⢸⡎⣿⡀⠀⠀⠀⢤⣔⡶⢲⠓⡴⢶⣢⡤⠀⠀⠀⣸⡟⣸⠿⣸⠎⡄⠀⡜⠀⠀⠀⢳⡘⡄⠀⠈⢧⠀⠀⠀⠀⠀
+⠀⢡⠋⠀⡸⢠⠓⡍⢻⣧⡙⠿⣶⣄⡀⠀⠘⢸⡻⣷⠹⣧⣶⠶⠟⣻⠏⢠⢾⣖⡳⣄⠻⣟⠿⢶⣴⡿⣱⡟⣆⡏⡰⠁⣼⠁⢀⣠⣴⡿⣻⣷⠀⠀⡜⣇⠀⠀⠀⠀
+⢠⠃⠀⢀⢃⠇⡰⠘⢥⡙⠿⣶⣌⡙⠻⢿⣶⣾⠁⠸⣧⣴⣶⠾⢿⠃⠀⠈⢿⣀⡟⠁⠀⠻⡿⣶⣬⣴⠟⠀⠘⣧⣥⣾⣶⡾⠿⢛⣥⣾⠟⢻⡇⠀⠘⡼⡆⠀⠀⠀
+⠃⠀⠀⡸⡌⣼⠇⠀⠀⠙⠦⣈⡙⠻⠿⣶⣶⡇⠀⠀⠉⠁⠀⡰⠃⠀⠀⢀⡟⠾⢳⠀⠀⠀⠙⣄⠉⠋⠀⠀⠀⢹⣭⣥⣴⣶⢿⠟⢻⣅⠝⢡⣸⡀⠀⢳⡸⡀⠀⠀
+⠀⠀⠀⣷⢰⢹⡂⠀⠀⠀⠀⣿⣷⣦⣧⣄⣀⡇⠀⠀⠀⢠⣊⡀⠀⠀⠀⡜⣇⣤⣌⢇⠀⠀⠀⠈⢢⠀⠀⠀⠀⣸⠉⣉⣱⣤⣼⡗⠊⠀⠀⠘⣧⢇⠀⢸⢣⠇⠀⠀
+⠀⠀⠀⡏⠇⡎⡇⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠉⠳⡄⢰⢱⠈⠛⠃⠘⡄⣰⠟⠉⠉⠀⠀⠀⡔⣽⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⡏⣾⡀⠀⡎⣾⠀⠀
+⠀⠀⠀⣿⠀⡇⡇⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣆⠠⠄⣀⡀⠀⠀⠘⡇⢸⢠⣖⣦⠀⣷⠋⠀⠀⢀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⡇⢱⡇⠀⡇⢸⠀⠀
+
+- ASCII art taken from <a href="https://emojicombos.com/anime-ascii-art" target="_blank">https://emojicombos.com/anime-ascii-art</a></pre>
+        </div>
+      </div>
+    </div>
+    <div id="footer">
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/resources/footer.html"); ?>
+    </div>
+  </body>
+</html>
