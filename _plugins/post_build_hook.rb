461492b18ea2c1096632724a36cefc2149b41840
TheRealOne78 <bajcsielias78@gmail.com>
2025-01-01 03:20:00 +0200

    add a hook for jekyll to npm install deps after build

--- _plugins/post_build_hook.rb
+++ _plugins/post_build_hook.rb
@@ -1,4 +1,5 @@
 Jekyll::Hooks.register :site, :post_write do |site|
   system("sh _scripts/post_sym_ln.sh")
   system("cd ./_site/resources/advguild/ && sh genhtml.sh")
+  system("cd ../npm && npm install")
 end
